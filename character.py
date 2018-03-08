c8d2d8a68f65fc25c3815a9bbdf1641da69a2d2b
andrewpmaus <andrewpmaus@gmail.com>
2018-03-07 21:21:47 -0500

    Added rect objects, hitBy, and grow

--- character.py
+++ character.py
@@ -3,15 +3,22 @@ from pygame.locals import *
 from colors import *
 
 class Character:
-    
+
     def __init__(self,x,y):
         # la di da di da
         self.x=x
         self.y=y
-        pass
+        self.width = 10
+        self.height = 10
+        self.rect =  pygame.Rect((self.x,self.y,self.width,self.height))
     # def ...
     def move(self,xdir,ydir):
         self.x+=xdir
         self.y+=ydir
     def draw(self, surf):
-        pygame.draw.rect(surf,RED,(self.x,self.y,10,10))
+        pygame.draw.rect(surf,RED,self.rect)
+    def grow(self):
+        self.width += 2
+        self.height += 2
+    def hitBy(self,rect):
+        return self.rect.colliderect(rect)
